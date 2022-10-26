e1810a6c26481a95ac10484e4e0376a3e7373cbc
Ali Mosaloo <m.mosalo1382@gmail.com>
2022-10-26 20:00:04 +0330

    debuging controller and makes code more beautiful than it used to be

--- bash/renamer.sh
+++ bash/renamer.sh
@@ -2,25 +2,37 @@
 address=$1
 command=$2
 controller=0
+#####################
 if [ "$command" = "-c" ] || [ "$command" = "--copy" ]; then
 controller=1
 fi 
+if [ $command = ""] || []; then
+controller=3
+fi
+#################
+
 if [ -d $address ]; then 
+
     date="$(date '+%Y-%m-%d')_"
+
+    if [ $controller == 3 ]; then
+        for document in $address/*
+        do
+            name=$(basename $document)
+            mv $document $address/$date$name
+            
+            echo "name: $name type: ${name##*.}  ->  changing name to: $date$name"
+        done
+    fi  ###
+
     if [ $controller == 1 ]; then
         
         for document in $address/*
         do
             name=$(basename $document)
             cp -f $document $address/$date$name
-            echo "copy:$name type:${name##*.} -> $date$name"
+            echo "name: $name type: ${name##*.}  ->  making copy: $date$name"
+            
         done
-    fi
-    if [ $controller == 0 ]; then
-        for document in $address/*
-        do
-            name=$(basename $document)
-            mv $document $address/$date$name
-        done
-        fi
+    fi ###
 fi
