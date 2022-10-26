6801209b7a04554298624ea51e050aa14ec00fd9
Ali Mosaloo <m.mosalo1382@gmail.com>
2022-10-26 19:42:10 +0330

    get file and rename and copying and representing result

--- bash/renamer.sh
+++ bash/renamer.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 address=$1
 command=$2
-if [ $command="-c" ] || [$command="--copy"]; then
-echo "this is complete "
+controller=0
+if [ "$command" = "-c" ] || [ "$command" = "--copy" ]; then
+controller=1
+fi 
+if [ -d $address ]; then 
+    date="$(date '+%Y-%m-%d')_"
+    if [ $controller == 1 ]; then
+        
+        for document in $address/*
+        do
+            name=$(basename $document)
+            cp -f $document $address/$date$name
+            echo "copy:$name type:${name##*.} -> $date$name"
+        done
+    fi
+    if [ $controller == 0 ]; then
+        for document in $address/*
+        do
+            name=$(basename $document)
+            mv $document $address/$date$name
+        done
+        fi
 fi
